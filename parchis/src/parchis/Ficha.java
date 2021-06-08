08158ddecff2d10ceb6cebcd7d8ba91fb2751c7f
AlbertVilla9 <82578544+AlbertVilla9@users.noreply.github.com>
2021-06-08 10:34:57 +0200

    Add files via upload

---
+++ parchis/src/parchis/Ficha.java
@@ -0,0 +1,16 @@
+package parchis;
+
+public class Ficha {
+	
+	private double y;
+	private double x;
+	private String nombre;
+	
+	
+	public Ficha(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+
+}
