0f45ff04a4756781c0be58de6c35b91c703d1439
AlbertVilla9 <82578544+AlbertVilla9@users.noreply.github.com>
2021-06-08 10:33:52 +0200

    Update Casilla.java

--- parchis/src/parchis/Casilla.java
+++ parchis/src/parchis/Casilla.java
@@ -4,5 +4,13 @@ public class Casilla {
 	
 	private double y;
 	private double x;
+	protected int tipo;
+	private static final int ALTOHORIZONTAL;
+	
+	public Casilla(double x, double y, int tipo) {
+		this.x = x;
+		this.y = y;
+		this.tipo=tipo;
+	}
 
 }
