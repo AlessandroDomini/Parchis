7b0f124ca6392fb84d39b846a23fd21b7977f658
AlessandroDomini <82506304+AlessandroDomini@users.noreply.github.com>
2021-06-10 08:58:28 +0200

    Update Ficha.java

--- parchis/src/parchis/Ficha.java
+++ parchis/src/parchis/Ficha.java
@@ -2,21 +2,32 @@ package parchis;
 
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class Ficha {
 	
+	private final int SIZE=46;
 	private double y;
 	private double x;
 	private String nombre;
+	protected String s="/Ficha_amarilla.png";
+
 	
+	private JLabel f1 = new JLabel("");
 	
-	public Ficha(double x, double y, JLabel f1, String s) {
+	public Ficha(double x, double y, JPanel p1) {
 		this.x = x;
 		this.y = y;
+		f1.setBounds(461, 438, SIZE, SIZE);
 		f1.setIcon(new ImageIcon(Game.class.getResource(s)));
+		p1.add(f1);
+
 	}
 	
-	
-	
+	public void moverFicha() {
+		
+		f1.setBounds(364, 560, 46, 46);	
+		
+	}
 
 }
