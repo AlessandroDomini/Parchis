74bee8c0a793d4dedb93e8cffda69cfaf9197c3c
AlessandroDomini <dominiblanchalessandro@gmail.com>
2021-06-12 18:16:00 +0200

    update
    Signed-off-by: AlessandroDomini <dominiblanchalessandro@gmail.com>

--- parchis/src/parchis/Casilla.java
+++ parchis/src/parchis/Casilla.java
@@ -2,15 +2,32 @@ package parchis;
 
 public class Casilla {
 	
-	private double y;
-	private double x;
+	private int y;
+	private int x;
 	protected int tipo;
-	private static final int ALTOHORIZONTAL;
+	private static final int ALTOHORIZONTAL=9;
 	
-	public Casilla(double x, double y, int tipo) {
+
+	public Casilla(int x, int y) {
 		this.x = x;
 		this.y = y;
 		this.tipo=tipo;
+	}
+	
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
 	}
 
 }
