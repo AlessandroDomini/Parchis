97106a5e35add0b9a0f1b80b316e97c3f039c4b4
AlessandroDomini <82506304+AlessandroDomini@users.noreply.github.com>
2021-06-18 00:09:07 +0200

    Update TurnosGame.java

--- parchis/src/parchis/TurnosGame.java
+++ parchis/src/parchis/TurnosGame.java
@@ -1,46 +1,48 @@
 package parchis;
 
 import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class TurnosGame extends Thread{
-	
-DataInputStream input = null;
-Game G1;
+public class TurnosGame extends Thread {
 
-public TurnosGame(DataInputStream input, Game G1) {
-	super();
-	this.input = input;
-	this.G1 = G1;
+	DataInputStream input = null;
+	Game G1;
 
-}
+	public TurnosGame(DataInputStream input, Game G1) {
+		super();
+		this.input = input;
+		this.G1 = G1;
 
-@Override
-public void run() {
-	while(true) {
-		try {
-			int ficha = input.readInt();
-			int casilla = input.readInt();
-			if(this.G1.who.contains("Yellow")){
-				this.G1.amarillas[ficha].moverFicha(this.G1.casillaA[casilla]);
-				
-			}else if(this.G1.who.contains("Red")) {
-				this.G1.rojas[ficha].moverFicha(this.G1.casillaR[casilla]);
-
-			}
-			this.G1.pasarTurno(true);
-			this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "************************************** \n");
-			this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "Your opponent has moved, is your turn. \n");
-
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
 	}
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				int ficha = input.readInt();
+				int casilla = input.readInt();
+				if (this.G1.who.contains("Yellow")) {
+					this.G1.amarillas[ficha].moverFicha(this.G1.casillaA[casilla]);
+					this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "*************************** \n");
+					this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "Yellow has moved the token "
+							+ (ficha + 1) + " to the cell " + (casilla + 4) + " is YOUR TURN\n");
+					this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "\n***************************");
+
+				} else if (this.G1.who.contains("Red")) {
+					this.G1.rojas[ficha].moverFicha(this.G1.casillaR[casilla]);
+					this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "*************************** \n");
+					this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "Red has moved the token "
+							+ (ficha + 1) + "  to the cell " + (casilla + 38) + " is YOUR TURN\n");
+					this.G1.area_instruct.setText(this.G1.area_instruct.getText() + "\n***************************");
+
+				}
+				this.G1.pasarTurno(true);
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
 }
-
-
-
-}
-
